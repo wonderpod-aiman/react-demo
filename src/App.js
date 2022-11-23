c4284f1cd4220549db398443b782cb25ee1f196e
Aiman Shamsiemon <maimansham@gmail.com>
2022-11-23 14:56:19 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [pokemonData, setPokemonData] = useState([])
+
+  useEffect(() => {
+    
+    const onScreenLoad = async () => {
+      //setPokemonData("a")
+      let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10000')
+      .then((response) => {
+          if (response.status !== 200){
+              alert("Something Went Wrong!");
+              return;
+          }
+          response.json().then(data => {setPokemonData(data.results)});
+
+        }
+      );
+
+      await response;
+    }
+
+    onScreenLoad();
+
+  }, [])
+
+  console.log(pokemonData.length)
+  if (pokemonData.length > 1){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        <li>
+          {pokemonData?.map(i => <ul>{i.name}</ul>)}
+        </li>
+  
+      </div>
+    );
+  }else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Waiting for data
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+  
+      </div>
+    );
+  }
+  
 }
 
 export default App;
